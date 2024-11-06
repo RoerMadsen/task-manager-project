ed6b723b917fb50afe1136b280e417bd5440a44c
RoerMadsen <amanda.vaergman@gmail.com>
2024-11-06 20:53:41 +0100

    fonte farver og opdatering af sass og api

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,13 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./style.scss";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Dette er h1</h1>
+      <h2>Dette er h2</h2>
+      <h3>Dette er h3</h3>
+      <p>Dette er p</p>
+      <p className="bold">Dette er p bold</p>
     </>
   );
 }
